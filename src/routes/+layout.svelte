5dcb0c38f1c06278465f62fd6d7c349065084ed3
Nahos <zsaquar1an.42@gmail.com>
2023-04-07 23:38:04 +0530

    feat: made initial website

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+  import hljs from 'highlight.js';
+  import 'highlight.js/styles/github-dark.css';
+  import { storeHighlightJs } from '@skeletonlabs/skeleton';
+
+  storeHighlightJs.set(hljs);
+</script>
+
+<slot />

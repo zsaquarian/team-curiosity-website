82e220fa80806689d2a4bf410d3c1a8b77664ed6
Nahos <zsaquar1an.42@gmail.com>
2023-04-08 10:27:51 +0530

    feat: add bibliography

---
+++ src/lib/Bibliography.svelte
@@ -0,0 +1,5 @@
+<ul>
+  <li><a href="https://www.elprocus.com/infrared-ir-sensor-circuit-and-working/">IR Sensor</a></li>
+  <li><a href="https://docs.arduino.cc/hardware/nano">Arduino Nano</a></li>
+  <li><a href="https://www.etechnophiles.com/l293d-motor-driver-module-ic-datasheet-pinouts/">Motor Driver</a></li>
+</ul>
